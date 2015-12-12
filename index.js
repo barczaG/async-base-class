7ad605bcf4ef91d4d2aa2f0a347a04922eb3b3c3
barczaG <gergo@barcza.hu>
2015-12-12 13:40:28 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const Promise = require('bluebird');
+const co = require('co');
+
+class AsyncClass {
+	constructor () {
+		var proto = Object.getPrototypeOf(this);
+		var classMethods = Object.getOwnPropertyNames(proto);
+		console.log(classMethods);
+		
+		classMethods.forEach(function(method) {
+		    var methodName = method;
+		    var fn = proto[methodName];
+		    
+		    if(methodName.endsWith('Async')) {
+		        var newKey = methodName.substring(0, methodName.length - 5);
+
+		        if( fn.constructor.name === 'GeneratorFunction') {
+		            proto[newKey] = co.wrap(proto[methodName]);
+		        } else {
+		            proto[newKey] = Promise.method(proto[methodName]);
+		        }
+		        
+		        delete proto[methodName];
+		    }
+		});
+	}
+}
+
+module.exports = AsyncClass;
