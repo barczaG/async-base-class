4b6f56160aeee20241ece8625a9d7a58484143d3
barczaG <gergo@barcza.hu>
2015-12-12 17:29:37 +0100

    Handle static methods

--- index.js
+++ index.js
@@ -3,28 +3,42 @@
 const Promise = require('bluebird');
 const co = require('co');
 
-class AsyncClass {
-	constructor () {
-		var proto = Object.getPrototypeOf(this);
-		var classMethods = Object.getOwnPropertyNames(proto);
-		
-		classMethods.forEach(function(method) {
-		    var methodName = method;
-		    var fn = proto[methodName];
-		    
-		    if(methodName.endsWith('Async')) {
-		        var newKey = methodName.substring(0, methodName.length - 5);
+function wrapFunctions(target, methodNames) {
+    _actualMethodKeys(target).forEach(function(key) {
+        let constructor = target[key].constructor.name;
 
-		        if( fn.constructor.name === 'GeneratorFunction') {
-		            proto[newKey] = co.wrap(proto[methodName]);
-		        } else {
-		            proto[newKey] = Promise.method(proto[methodName]);
-		        }
-		        
-		        delete proto[methodName];
-		    }
-		});
-	}
+        if (methodNames) {
+            if (methodNames.indexOf(key) === -1) return;
+        } else if (!key.endsWith('Async') && constructor !== 'GeneratorFunction') {
+            return;
+        }
+        var newKey = key.substring(0, key.length - 5);
+        if (target[key].constructor.name === 'GeneratorFunction') {
+            target[newKey] = co.wrap(target[key]);
+        } else {
+            target[newKey] = Promise.method(target[key]);
+        }
+
+        delete target[key];
+    });
+}
+
+
+function _actualMethodKeys(target) {
+    return Object.getOwnPropertyNames(target)
+    .filter(key => {
+        var propertyDescriptor = Object.getOwnPropertyDescriptor(target, key);
+        return !propertyDescriptor.get && !propertyDescriptor.set;
+    })
+    .filter(key => typeof target[key] === 'function');
+}
+
+class AsyncClass {
+    constructor () {
+        var proto = Object.getPrototypeOf(this);
+        wrapFunctions(proto);
+        wrapFunctions(this);
+    }
 }
 
 module.exports = AsyncClass;
