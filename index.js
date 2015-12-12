ebd8ee3d62b94b1c9e29273191dbe917a4dbe302
barczaG <gergo@barcza.hu>
2015-12-12 20:44:54 +0100

    Use wrapper syntax instead of class extend

--- index.js
+++ index.js
@@ -1,44 +1,105 @@
 'use strict';
-
 const Promise = require('bluebird');
 const co = require('co');
 
-function wrapFunctions(target, methodNames) {
-    _actualMethodKeys(target).forEach(function(key) {
-        let constructor = target[key].constructor.name;
-
-        if (methodNames) {
-            if (methodNames.indexOf(key) === -1) return;
-        } else if (!key.endsWith('Async') && constructor !== 'GeneratorFunction') {
-            return;
-        }
-        var newKey = key.substring(0, key.length - 5);
-        if (target[key].constructor.name === 'GeneratorFunction') {
-            target[newKey] = co.wrap(target[key]);
-        } else {
-            target[newKey] = Promise.method(target[key]);
-        }
-
-        delete target[key];
-    });
+/**
+ * Wraps static and instance methods whose name ends with Async, or are
+ * GeneratorFunctions. Any GeneratorFunction is wrapped with
+ * bluebird.coroutine(), and others with bluebird.method(). Accepts an optional
+ * array of method names, wrapping only those found in the array, and disabling
+ * the Async suffix check. Returns the class.
+ *
+ * @param   {function} klass         The class to wrap
+ * @param   {string[]} [methodNames] Optional array of method names
+ * @returns {function} The supplied class
+ * @throws  {Error}    If methodNames is provided, but is not an array
+ */
+function wrap(klass, methodNames) {
+  validateMethodNames(methodNames);
+  wrapStaticMethods(klass, methodNames);
+  wrapInstanceMethods(klass, methodNames);
+  return klass;
 }
 
+/**
+ * Wraps static methods whose name ends with Async or are GeneratorFunctions.
+ * Any GeneratorFunction is wrapped with bluebird.coroutine(), and others with
+ * bluebird.method(). Accepts an optional array of method names, wrapping only
+ * those found in the array, and disabling the Async suffix check. Returns the
+ * class.
+ *
+ * @param   {function} klass         The class to wrap
+ * @param   {string[]} [methodNames] Optional array of method names
+ * @returns {function} The supplied class
+ * @throws  {Error}    If methodNames is provided, but is not an array
+ */
+function wrapStaticMethods(klass, methodNames) {
+  validateMethodNames(methodNames);
+  wrapFunctions(klass, methodNames);
+  return klass;
+}
+
+/**
+ * Wraps instance methods whose name ends with Async, or are GeneratorFunctions.
+ * Any GeneratorFunction is wrapped with bluebird.coroutine(), and others with
+ * bluebird.method(). Accepts an optional array of method names, wrapping only
+ * those found in the array, and disabling the Async suffix check. Returns the
+ * class.
+ *
+ * @param   {function} klass         The class to wrap
+ * @param   {string[]} [methodNames] Optional array of method names
+ * @returns {function} The supplied class
+ * @throws  {Error}    If methodNames is provided, but is not an array
+ */
+function wrapInstanceMethods(klass, methodNames) {
+  validateMethodNames(methodNames);
+  wrapFunctions(klass.prototype, methodNames);
+  return klass;
+}
+
+/**
+ * Helper function that validates the methodNames parameter.
+ *
+ * @param {string[]} [methodNames] Optional array of method names
+ * @throws {Error}   If methodNames is provided, but is not an array
+ */
+function validateMethodNames(methodNames) {
+  if (methodNames && !(methodNames instanceof Array)) {
+    throw new Error('Optional methodNames should be an array if provided');
+  }
+}
+
+function wrapFunctions(target, methodNames) {
+  _actualMethodKeys(target).forEach(function(key) {
+    let constructor = target[key].constructor.name;
+
+    if (methodNames) {
+      if (methodNames.indexOf(key) === -1) return;
+    } else if (!key.endsWith('Async') && constructor !== 'GeneratorFunction') {
+      return;
+    }
+
+    var newKey = key.substring(0, key.length - 5);
+    if (target[key].constructor.name === 'GeneratorFunction') {
+      target[newKey] = Promise.coroutine(target[key]);
+    } else {
+      target[newKey] = Promise.method(target[key]);
+    }
+    delete target[key];
+  });
+}
 
 function _actualMethodKeys(target) {
-    return Object.getOwnPropertyNames(target)
+  return Object.getOwnPropertyNames(target)
     .filter(key => {
-        var propertyDescriptor = Object.getOwnPropertyDescriptor(target, key);
-        return !propertyDescriptor.get && !propertyDescriptor.set;
+      var propertyDescriptor = Object.getOwnPropertyDescriptor(target, key);
+      return !propertyDescriptor.get && !propertyDescriptor.set;
     })
     .filter(key => typeof target[key] === 'function');
 }
 
-class AsyncClass {
-    constructor () {
-        var proto = Object.getPrototypeOf(this);
-        wrapFunctions(proto);
-        wrapFunctions(this);
-    }
-}
-
-module.exports = AsyncClass;
+module.exports = {
+  wrap,
+  wrapStaticMethods,
+  wrapInstanceMethods
+};
